c16ea0c3c1347f199c99f2cc8a85d2cbd742da3d
Saimon <saimon.ventura@freshbooks.com>
2021-12-20 18:39:03 -0300

    gogo

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useState } from "react";
 
 export default function Home() {
-  const [key, setKey] = useState("");
   const [token, setToken] = useState("");
 
   return (
@@ -19,27 +17,17 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Recaptcha</h1>
 
-        <input
-          onChange={({ currentTarget: { value } }) => {
-            setKey(value);
+        <ReCAPTCHA
+          onExpired={() => {
+            setToken("");
           }}
-          type="text"
-          placeholder="Public Key"
+          onChange={(token) => {
+            navigator.clipboard.writeText(token);
+            setToken(token);
+            snackbarControl.show("Token copiado para o clipboard");
+          }}
+          sitekey={"6LcwawIaAAAAAHmTEOLMqjIO0lEIEjV1lwSajYqt"}
         />
-
-        {key ? (
-          <ReCAPTCHA
-            onExpired={() => {
-              setToken("");
-            }}
-            onChange={(token) => {
-              navigator.clipboard.writeText(token);
-              setToken(token);
-              snackbarControl.show("Token copiado para o clipboard");
-            }}
-            sitekey={key}
-          />
-        ) : null}
 
         {token ? (
           <>
@@ -48,19 +36,6 @@ export default function Home() {
           </>
         ) : null}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
